6747c63a7fd67c5843f68bf5564f4d1b38cbfaea
LYJ22 <dora478@naver.com>
2024-09-12 01:22:04 +0900

    button component1

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const Button = ({ text }) => {
-  return <button>{text}</button>;
+// text : 버튼 내용
+// width : 수정 중
+// option : 정해진 buttonOption의 인덱스 or 직접 tailwind 문구 입력
+const Button = ({ text, width, option }) => {
+  const buttonOption = [
+   " bg-black text-white h-14 rounded-lg text-lg", //0. 구매하기
+   " bg-white h-14 rounded-lg border border-gray-300 text-lg",  //1. 옵션 선택 닫기
+   " bg-white h-13 rounded px-3 border border-blue-200 text-blue-500 text-sm" //2. 상세페이지 버튼
+  ]
+  let btnClassName = "w-full py-4 flex justify-center itmes-center cursor-pointer font-semibold";
+
+  console.log(typeof(option))
+  btnClassName = btnClassName + " max-w-"+width;
+  typeof(option)==="number" ? btnClassName = btnClassName + buttonOption[option] : btnClassName = btnClassName + " "+ option;
+
+  return <button className={btnClassName}>{text}</button>;
 };
 
 export default Button;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Button from "../components/common/Button"
 
 const MainPage = () => {
-  return <div>main</div>;
+  return <div>
+    <h2>main</h2>
+    <Button text={"구매하기"} width={32} option={0}/>
+    <Button text={"옵션선택"} width={36} option={1}/>
+    <Button text={"test"} width={36} option={"bg-red-100 text-green-600"}/>
+  </div>;
 };
 
 export default MainPage;
