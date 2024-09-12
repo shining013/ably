e2b7c81da30bf0e7f115bf4ffabbb3ad020d8c8d
LYJ22 <pippi478@gmail.com>
2024-09-12 10:10:00 +0900

    button component2

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 
 // text : 버튼 내용
-// width : 수정 중
-// option : 정해진 buttonOption의 인덱스 or 직접 tailwind 문구 입력
+// width : 버튼 길이(px)
+// option : 등록된 buttonOption의 인덱스 or 직접 tailwind 설정
 const Button = ({ text, width, option }) => {
   const buttonOption = [
-   " bg-black text-white h-14 rounded-lg text-lg", //0. 구매하기
-   " bg-white h-14 rounded-lg border border-gray-300 text-lg",  //1. 옵션 선택 닫기
-   " bg-white h-13 rounded px-3 border border-blue-200 text-blue-500 text-sm" //2. 상세페이지 버튼
-  ]
-  let btnClassName = "w-full py-4 flex justify-center itmes-center cursor-pointer font-semibold";
+    " bg-black text-white h-14 rounded-lg text-lg", //0. 구매하기
+    " bg-white h-14 rounded-lg border border-gray-300 text-lg", //1. 옵션 선택 닫기
+    " bg-white h-13 rounded px-3 border border-blue-200 text-blue-500 text-sm", //2. 상세페이지 버튼
+  ];
 
-  console.log(typeof(option))
-  btnClassName = btnClassName + " max-w-"+width;
-  typeof(option)==="number" ? btnClassName = btnClassName + buttonOption[option] : btnClassName = btnClassName + " "+ option;
+  // 버튼 기본 설정
+  let btnClassName =
+    "w-full py-4 flex justify-center itmes-center cursor-pointer font-semibold";
+  const btnWidth = width + "px";
 
-  return <button className={btnClassName}>{text}</button>;
+  typeof option === "number"
+    ? (btnClassName = btnClassName + buttonOption[option])
+    : (btnClassName = btnClassName + " " + option);
+
+  return (
+    <div style={{ width: btnWidth }}>
+      <button className={btnClassName}>{text}</button>
+    </div>
+  );
 };
 
 export default Button;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import Button from "../components/common/Button"
+import Button from "../components/common/Button";
 
 const MainPage = () => {
-  return <div>
-    <h2>main</h2>
-    <Button text={"구매하기"} width={32} option={0}/>
-    <Button text={"옵션선택"} width={36} option={1}/>
-    <Button text={"test"} width={36} option={"bg-red-100 text-green-600"}/>
-  </div>;
+  return (
+    <div>
+      <h2>main</h2>
+      <Button text={"구매하기"} width={"400"} option={0} />
+      <Button text={"옵션선택"} width={500} option={1} />
+      <Button text={"test"} width={600} option={"bg-red-100 text-green-600"} />
+    </div>
+  );
 };
 
 export default MainPage;
