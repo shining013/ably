bf9ee27e4a5d560d1ba22d7364756ce93c21c60f
LYJ22 <pippi478@gmail.com>
2024-09-12 17:48:09 +0900

    cart topbar1

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import CartTopBar from "../components/cart/CartTopBar";
+import CartSelectBar from "../components/cart/CartSelectBar";
 
 const CartPage = () => {
   return (
     <div>
-      
+      <CartTopBar />
+      <CartSelectBar />
     </div>
-  )
-}
+  );
+};
 
-export default CartPage
+export default CartPage;
