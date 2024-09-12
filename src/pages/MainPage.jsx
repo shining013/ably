f5640634bb245a2ce2dad94ad55a01263fe01169
이효원 <ymj07168@gmail.com>
2024-09-12 15:17:44 +0900

    fix: conflict resolve

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Card from "../components/common/Card";
 import CardList from "../components/common/CardList";
 
 const MainPage = () => {
   return (
     <div>
-
       <CardList />
     </div>
   );
