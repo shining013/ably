7b43297633e95fcdfcddc9f4c153a45d59f51c90
junsoo1003 <royalex@dgu.ac.kr>
2024-09-13 16:07:44 +0900

    feat : 슬라이드 구현중

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import CardList from "../components/common/CardList";
+import SlideComp from "../components/SlideComp";
 
 const MainPage = () => {
   return (
     <div>
+      <SlideComp />
       <CardList />
     </div>
   );
