0cd4d8ba4ed0cabc1a2981e0fbcaea10ecad04c0
shining013 <nevertheless113@naver.com>
2024-09-13 20:22:47 +0900

    [Chore]:MainPage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import CardList from "../components/common/CardList";
-
+import SlideComp from "../components/SlideComp";
 import CategoryList from "../components/main/CategoryList";
 import BottomBar from "../components/main/Bottomnav";
 
 const MainPage = () => {
   return (
     <div style={{ paddingTop: "120px" }}>
-      <CategoryList />
       <SlideComp />
+      <CategoryList />
       <CardList />
       <BottomBar />
     </div>
