5bf94d0fdf9becc4fb26bd5f96853f1224790630
shining013 <nevertheless113@naver.com>
2024-09-15 21:44:18 +0900

    Searchbar

--- src/components/main/CategoryList.jsx
+++ src/components/main/CategoryList.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 function CategoryList() {
   const categories = [
-    { id: 1, name: "남자패션", imageSrc: "./images/Cate1.png" },
-    { id: 2, name: "의류", imageSrc: "./images/Cate2.png" },
-    { id: 3, name: "주얼리", imageSrc: "./images/Cate3.png" },
-    { id: 4, name: "패션소품", imageSrc: "./images/Cate4.png" },
-    { id: 5, name: "빅사이즈", imageSrc: "./images/Cate5.png" },
-    { id: 6, name: "신발", imageSrc: "./images/Cate6.png" },
-    { id: 7, name: "디지털/핸드폰", imageSrc: "./images/Cate7.png" },
-    { id: 8, name: "가방", imageSrc: "./images/Cate8.png" },
-    { id: 9, name: "뷰티", imageSrc: "./images/Cate9.png" },
-    { id: 10, name: "라이프", imageSrc: "./images/Cate10.png" },
+    { id: 1, name: "남자패션", imageSrc: "/images/Cate1.png" },
+    { id: 2, name: "의류", imageSrc: "/images/Cate2.png" },
+    { id: 3, name: "주얼리", imageSrc: "/images/Cate3.png" },
+    { id: 4, name: "패션소품", imageSrc: "/images/Cate4.png" },
+    { id: 5, name: "빅사이즈", imageSrc: "/images/Cate5.png" },
+    { id: 6, name: "신발", imageSrc: "/images/Cate6.png" },
+    { id: 7, name: "디지털/핸드폰", imageSrc: "/images/Cate7.png" },
+    { id: 8, name: "가방", imageSrc: "/images/Cate8.png" },
+    { id: 9, name: "뷰티", imageSrc: "/images/Cate9.png" },
+    { id: 10, name: "라이프", imageSrc: "/images/Cate10.png" },
   ];
 
   const testStyle = {
@@ -66,8 +66,7 @@ function CategoryList() {
                 display: "flex",
                 flexDirection: "column",
               }}
-            >
-            </div>
+            ></div>
           </div>
         </div>
       ))}
