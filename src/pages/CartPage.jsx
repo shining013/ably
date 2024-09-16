4af7aac6ee5d5562cf2e202f2add968419c88e07
LYJ22 <dora478@naver.com>
2024-09-16 17:51:20 +0900

    cart item list1 (no merging)

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import CartTopBar from "../components/cart/CartTopBar";
 import CartSelectBar from "../components/cart/CartSelectBar";
 import CartEmpty from "../components/cart/CartEmpty";
+import CartItem from "../components/cart/CartItem";
 
 const CartPage = () => {
   const [itemlist, setItemlist] = useState(["hi", "hello", "bye"]);
@@ -19,30 +20,10 @@ const CartPage = () => {
     return (
       <div className="relative h-screen" style={{ top: "102px" }}>
         <CartSelectBar totalItem={itemlist.length} selectItem={buylist.size} />
-        <div>
-          <p>(회사명) 배송상품</p>
-          <div className="flex w-full py-4 pr-6">
-            <input
-              type="checkbox"
-              className="w-4 h-4 mr-2 self-center"
-              style={{ color: "#9e9e9e" }}
-            ></input>
-            <img
-              src="./images/Cate2.png"
-              style={{ width: "90px", height: "90px" }}
-            ></img>
-            <div>
-              <p>(회사명)</p>
-              <p>(상품명)</p>
-              <div>
-                <p>(정상가)</p>
-                <p>(할인가)</p>
-              </div>
-            </div>
-            <div>X</div>
-          </div>
+        <CartItem />
+        <CartItem />
+        <CartItem />
         </div>
-      </div>
     );
   };
 
