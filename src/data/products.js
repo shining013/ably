6c8463d3bb1bd7eb7205f9f37da7607d32a86d66
이효원 <ymj07168@gmail.com>
2024-09-19 16:04:03 +0900

    feat: 더미 데이터 형식 변경

--- src/data/products.js
+++ src/data/products.js
@@ -1,7 +1,15 @@
 const products = [
-    {
-        id: 3342, name: '23423', primeCategory: 'jwerly', subCategory: 'earing', description: '234', price: 123, discount: 10,
-        company: 'dd',
-        imgSrc: 'ddd',
-    },
-]
+  {
+    id: 3342,
+    name: "23423",
+    primeCategory: "jwerly",
+    subCategory: "earing",
+    description: "234",
+    price: 123,
+    discount: 10,
+    companyId: 3342,
+    companyName: "22sdfsd3",
+    imgSrc: "ddd",
+    totalSales: 3342,
+  },
+];
