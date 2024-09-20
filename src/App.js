04dd8537e55fb630eff130668bb4c9d5592f3936
이효원 <ymj07168@gmail.com>
2024-09-20 23:00:10 +0900

    fix: redux 설정 오류 해결

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { ThemeProvider } from "@material-tailwind/react";
 import { SnackbarProvider } from "notistack";
 import React from "react";
-import { createStoreHook, Provider } from "react-redux";
+import { Provider } from "react-redux";
 import Router from "src/Router";
 import reducer from "./redux/reducer";
+import { createStore } from "redux";
 
-const store = createStoreHook(reducer);
+const store = createStore(reducer);
 const App = () => {
   return (
-    <ThemeProvider store={store}>
+    <ThemeProvider>
       <SnackbarProvider />
-      <Provider>
+      <Provider store={store}>
         <Router />
       </Provider>
     </ThemeProvider>
