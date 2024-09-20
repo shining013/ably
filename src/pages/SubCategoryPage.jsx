15c4366155c1e3697de7bfcdda4ace2d6511c921
chaelin2 <gcl693427@gmail.com>
2024-09-21 00:11:50 +0900

    feat: 서브 카테고리 상세페이지 구현

--- src/pages/SubCategoryPage.jsx
+++ src/pages/SubCategoryPage.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import CardList from "src/components/common/CardList";
 import Card from "src/components/common/Card";
 import SubProductList from "src/components/subcategory/SubProductList";
+import TopInfo from "src/components/common/TopInfo";
+import TopNavbar from "src/components/common/TopNavbar";
+import RankingBar from "src/components/subcategory/RankingBar";
 
 function CategoryPage() {
   return (
     <div>
-      CategoryPage
+      <TopInfo />
+      <TopNavbar title="귀걸이" />
+      {/* 실시간 귀걸이 랭킹 전체보기 바  */}
+      <RankingBar />
+
       <SubProductList />
     </div>
   );
