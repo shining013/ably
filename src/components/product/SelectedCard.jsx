c6a31c8822accdee1c867d6371b81f0058e7033c
이효원 <ymj07168@gmail.com>
2024-09-20 16:13:44 +0900

    feat: 옵션 선택 기능 추가

--- src/components/product/SelectedCard.jsx
+++ src/components/product/SelectedCard.jsx
@@ -3,7 +3,16 @@ import deleteIcon from "src/assets/icons/delete.svg";
 import minusIcon from "src/assets/icons/minus.svg";
 import plusIcon from "src/assets/icons/plus.svg";
 
-const SelectedCard = ({ color, size, delivery, num, price = 0 }) => {
+const SelectedCard = ({
+  color,
+  size,
+  delivery,
+  num,
+  price = 0,
+  onDelete,
+  onPlus,
+  onMinus,
+}) => {
   return (
     <div className="flex flex-col gap-9 rounded-[4px] border border-gray-30 p-[18px]">
       <div className="flex flex-row justify-between items-start">
@@ -14,21 +23,33 @@ const SelectedCard = ({ color, size, delivery, num, price = 0 }) => {
           <div className="text-subtitle4 text-gray-50">{delivery}</div>
         </div>
         <button className="">
-          <img src={deleteIcon} alt="delete" />
+          <img
+            src={deleteIcon}
+            alt="delete"
+            onClick={() => onDelete({ color: color, size: size })}
+          />
         </button>
       </div>
       <div className="flex flex-row justify-between items-center">
         <div className="flex flex-row gap-2 items-center">
           <button className="border p-[5px] rounded-[50%] bg-gray-20">
-            <img src={minusIcon} alt="minus" />
+            <img
+              src={minusIcon}
+              alt="minus"
+              onClick={() => onMinus({ color: color, size: size })}
+            />
           </button>
           <div className="text-subtitle2 text-gray-70">{num}</div>
           <button className="border p-[5px] rounded-[50%]">
-            <img src={plusIcon} alt="plus" />
+            <img
+              src={plusIcon}
+              alt="plus"
+              onClick={() => onPlus({ color: color, size: size })}
+            />
           </button>
         </div>
         <div className="text-subtitle2 text-gray-70">
-          {price.toLocaleString()}원
+          {(price * num).toLocaleString()}원
         </div>
       </div>
     </div>
