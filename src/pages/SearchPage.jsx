2a9706a8b6179f75a509afbbc47efd3c3d8531df
shining013 <nevertheless113@naver.com>
2024-09-20 17:34:59 +0900

    feat: searchpagebar 생성

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import SearchPageBar from "src/components/main/SearchPageBar";
 
 function SearchPage() {
   return (
-    <div>SearchPage</div>
-  )
+    <>
+      <div
+        className="flex top-0 left-0 sticky w-full items-center justify-center flex-col"
+        style={{
+          zIndex: "999",
+        }}
+      >
+        <SearchPageBar className="flex justify-center" />
+      </div>
+    </>
+  );
 }
 
-export default SearchPage
+export default SearchPage;
