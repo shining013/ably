13086253bf6bf77a66adc8b3a6cd8f534f373a97
shining013 <nevertheless113@naver.com>
2024-09-20 17:53:05 +0900

    feat:searchpage

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,16 +1,15 @@
 import React from "react";
+import PopularSearchBar from "src/components/main/PopularSearchBar";
 import SearchPageBar from "src/components/main/SearchPageBar";
 
 function SearchPage() {
   return (
     <>
-      <div
-        className="flex top-0 left-0 sticky w-full items-center justify-center flex-col"
-        style={{
-          zIndex: "999",
-        }}
-      >
+      <div className="flex top-0 left-0 sticky w-full items-center justify-center flex-col">
         <SearchPageBar className="flex justify-center" />
+      </div>
+      <div style={{ paddingTop: "10px" }}>
+        <PopularSearchBar className="flex justify-center" />
       </div>
     </>
   );
