2674bc2230b916f5d649282a635932a6c7df040f
chaelin2 <gcl693427@gmail.com>
2024-09-23 20:21:07 +0900

    feat:서브카테고리 데이터 필터링

--- src/components/subcategory/RankingBar.jsx
+++ src/components/subcategory/RankingBar.jsx
@@ -3,12 +3,27 @@ import { useParams } from "react-router-dom";
 
 function RankingBar() {
   const { subcategoryId } = useParams();
+  let category = "";
+  if (subcategoryId === "earing") {
+    category = "귀걸이";
+  } else if (subcategoryId === "ring") {
+    category = "반지";
+  } else if (subcategoryId === "necklace") {
+    category = "목걸이";
+  } else if (subcategoryId === "bracelet") {
+    category = "팔찌";
+  } else if (subcategoryId === "anklet") {
+    category = "발찌";
+  } else if (subcategoryId === "jewerly-box") {
+    category = "보석함";
+  }
+
   return (
     <>
       <div>
         <ul className="content-center text-body3 py-5 px-2.5 flex justify-between items-center z-0 relative">
           <li className="text-h5 text-gray-70 font-semibold">
-            실시간 {subcategoryId} 랭킹
+            실시간 {category} 랭킹
           </li>
 
           <li className="self-auto cursor-pointer text-blue-700">
