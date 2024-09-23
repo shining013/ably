1de9a5d223eac20e39165f48c0bfd600b2c3ccba
chaelin2 <gcl693427@gmail.com>
2024-09-23 19:27:02 +0900

    feat:카드 데이터 필터링

--- src/components/subcategory/SubProductList.jsx
+++ src/components/subcategory/SubProductList.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 import Card from "src/components/common/Card";
 import { products } from "src/data/products";
 
 const SubProductList = () => {
+  let subProduct = "earing";
+
+  // subcategoryId === 'earing'
+  const { subcategoryId } = useParams();
+  let productData = [];
+
+  console.log(products.filter((item) => item.subCategory === subcategoryId));
+  productData = products.filter((item) => item.subCategory === subcategoryId);
+  console.log("productData: ", productData);
   return (
-    <div className="flex flew-row gap-3 max-w-[600px] w-full">
-      {products.map((item) => (
+    <div className="grid grid-cols-2 gap-3 max-w-[600px] w-full px-2">
+      {productData?.map((item) => (
         <Card
           imgSrc={item.imgSrc}
           discount={item.discount}
