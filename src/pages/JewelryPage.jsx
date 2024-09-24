064ecb08235e1cf176441984efb908f2242f8f8f
junsoo1003 <royalex@dgu.ac.kr>
2024-09-24 15:37:02 +0900

    fix: jewelry 페이지 좌우 패딩 범위 변경

--- src/pages/JewelryPage.jsx
+++ src/pages/JewelryPage.jsx
@@ -53,80 +53,80 @@ const JewelryPage = () => {
         <TopInfo />
         <TopNavbar title={"주얼리"} />
       </div>
-      <div className="px-2">
-        <div className="grid grid-cols-5 p-3 gap-2">
-          {jewelryTypes.map((type) => (
-            <JewelryType
-              key={type.imgSrc}
-              imgsrc={type.imgSrc}
-              typeName={type.typeName}
-              path={type.path}
-            />
-          ))}
-        </div>
+
+      <div className="grid grid-cols-5 p-3 gap-2">
+        {jewelryTypes.map((type) => (
+          <JewelryType
+            key={type.imgSrc}
+            imgsrc={type.imgSrc}
+            typeName={type.typeName}
+            path={type.path}
+          />
+        ))}
+      </div>
+      <div
+        style={{
+          height: "1px",
+          minHeight: "1px",
+          backgroundColor: "rgb(238, 238, 238)",
+        }}
+      ></div>
+      <div
+        style={{
+          height: "7px",
+          minHeight: "7px",
+          backgroundColor: "rgb(245, 245, 245)",
+        }}
+      ></div>
+      <div
+        className="flex gap-7 px-3"
+        style={{
+          borderBottomColor: "rgb(238, 238, 238)",
+          borderBottomStyle: "solid",
+          borderBottomWidth: "0.8px",
+        }}
+      >
         <div
+          className="cursor-pointer py-3 text-center text-sm"
           style={{
-            height: "1px",
-            minHeight: "1px",
-            backgroundColor: "rgb(238, 238, 238)",
+            borderBottom: selectedButton === "all" ? "2px solid black" : "none",
+            fontWeight: selectedButton === "all" ? "600" : "400",
+            color:
+              selectedButton === "all"
+                ? "rgb(31, 31, 31)"
+                : "rgb(119, 119, 119)",
           }}
-        ></div>
-        <div
-          style={{
-            height: "7px",
-            minHeight: "7px",
-            backgroundColor: "rgb(245, 245, 245)",
-          }}
-        ></div>
-        <div
-          className="flex gap-7 px-3"
-          style={{
-            borderBottomColor: "rgb(238, 238, 238)",
-            borderBottomStyle: "solid",
-            borderBottomWidth: "0.8px",
+          onClick={() => {
+            setSelectedButton("all");
           }}
         >
-          <div
-            className="cursor-pointer py-3 text-center text-sm"
-            style={{
-              borderBottom:
-                selectedButton === "all" ? "2px solid black" : "none",
-              fontWeight: selectedButton === "all" ? "600" : "400",
-              color:
-                selectedButton === "all"
-                  ? "rgb(31, 31, 31)"
-                  : "rgb(119, 119, 119)",
-            }}
-            onClick={() => {
-              setSelectedButton("all");
-            }}
-          >
-            전체
-          </div>
-          <div
-            className="cursor-pointer py-3 text-center text-sm"
-            style={{
-              borderBottom:
-                selectedButton === "brand" ? "2px solid black" : "none",
-              fontWeight: selectedButton === "brand" ? "600" : "400",
-              color:
-                selectedButton === "brand"
-                  ? "rgb(31, 31, 31)"
-                  : "rgb(119, 119, 119)",
-            }}
-            onClick={() => {
-              setSelectedButton("brand");
-            }}
-          >
-            브랜드
-          </div>
+          전체
         </div>
-        <div className="flex gap-3 pt-3 pb-[11px] px-4">
-          <JewelryFilter title="추천순"></JewelryFilter>
-          <JewelryFilter title="가격"></JewelryFilter>
-          <JewelryFilter title="색상"></JewelryFilter>
-          <JewelryFilter title="키·연령"></JewelryFilter>
+        <div
+          className="cursor-pointer py-3 text-center text-sm"
+          style={{
+            borderBottom:
+              selectedButton === "brand" ? "2px solid black" : "none",
+            fontWeight: selectedButton === "brand" ? "600" : "400",
+            color:
+              selectedButton === "brand"
+                ? "rgb(31, 31, 31)"
+                : "rgb(119, 119, 119)",
+          }}
+          onClick={() => {
+            setSelectedButton("brand");
+          }}
+        >
+          브랜드
         </div>
+      </div>
+      <div className="flex gap-3 pt-3 pb-[11px] px-4">
+        <JewelryFilter title="추천순"></JewelryFilter>
+        <JewelryFilter title="가격"></JewelryFilter>
+        <JewelryFilter title="색상"></JewelryFilter>
+        <JewelryFilter title="키·연령"></JewelryFilter>
+      </div>
+      <div className="px-2">
         <RecommendBox />
         <CardList />
       </div>
